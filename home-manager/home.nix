570d49cabe048fb2a6135cc8672f5900f4ca795f
hewenjin <hewenjin94@outlook.com>
2023-12-10 21:03:58 +0800

    fix: nvim config path

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -218,7 +218,7 @@
       python311Packages.pip
     ];
   };
-  xdg.configFile.nvim.source = ../nvim;
+  xdg.configFile.nvim.source = ./nvim;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
