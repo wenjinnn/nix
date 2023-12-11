ba77f89e8502cf88f627948da49dbea9fcd7b77e
wenjin <hewenjin94@outlook.com>
2023-12-11 22:01:51 +0800

    add zshrc config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -180,6 +180,10 @@
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
+    history = {
+      size = 99999;
+      ignoreAllDups = true;
+    };
     syntaxHighlighting = {
       enable = true;
     };
@@ -187,11 +191,22 @@
       enable = true;
       plugins = [
         "git"
+        "svn"
+        "mvn"
+        "docker"
+        "wd"
+        "z"
+        "history"
+        "extract"
+        "fzf"
         "sudo"
       ];
     };
     shellAliases = {
       lg = "lazygit";
+      ll = "ls -lah";
+      py = "python";
+      rr = "ranger";
     };
     plugins = [
       {
@@ -205,6 +220,74 @@
         file = ".p10k.zsh";
       }
     ];
+    initExtra = ''
+      bindkey '^ ' autosuggest-accept
+      PROXY_ENV=(http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY)
+      NO_PROXY_ENV=(no_proxy NO_PROXY)
+      proxy_value=http://127.0.0.1:7890
+      no_proxy_value=localhost,127.0.0.1,localaddress,.localdomain.com,10.96.0.0/12,192.168.99.0/24,192.168.39.0/24,192.168.49.2/24
+
+      proxyIsSet(){
+          for envar in $PROXY_ENV
+          do
+              eval temp=$(echo \$$envar)
+              if [ $temp ]; then
+                  return 0
+              fi
+          done
+          return 1
+
+      }
+
+      assignProxy(){
+          for envar in $PROXY_ENV
+          do
+             export $envar=$1
+          done
+          for envar in $NO_PROXY_ENV
+          do
+             export $envar=$2
+          done
+          echo "set all proxy env done"
+          echo "proxy value is:"
+          echo ''${proxy_value}
+          echo "no proxy value is:"
+          echo ''${no_proxy_value}
+      }
+
+      clrProxy(){
+          for envar in $PROXY_ENV
+          do
+              unset $envar
+          done
+          echo "cleaned all proxy env"
+      }
+
+      # toggleProxy
+      tp(){
+          if proxyIsSet 
+          then
+              clrProxy
+          else
+              # user=YourUserName
+              # read -p "Password: " -s pass &&  echo -e " "
+              # proxy_value="http://$user:$pass@ProxyServerAddress:Port"
+              assignProxy $proxy_value $no_proxy_value
+          fi
+      }
+
+      timestamp(){
+          current=`date "+%Y-%m-%d %H:%M:%S"`
+          if [ $1 ] ;then
+              current=$1
+              echo $current
+          fi
+
+          timeStamp=`date -d "$current" +%s`
+          currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
+          echo $currentTimeStamp
+      }
+      '';
   };
 
   programs.neovim = {
