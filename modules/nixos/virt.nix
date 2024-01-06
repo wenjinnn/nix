ac1232ff6197be2012f0a964d0403a5c871d453e
wenjin <hewenjin94@outlook.com>
2024-01-06 15:34:00 +0800

    add quickemu

--- modules/nixos/virt.nix
+++ modules/nixos/virt.nix
@@ -1,4 +1,15 @@
 {
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    quickemu
+    quickgui
+  ];
   # virtualisation
   programs.virt-manager.enable = true;    
   virtualisation.libvirtd.enable = true;
