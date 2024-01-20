ce4993415a26d742fe784e18749012d0282569d5
wenjin <hewenjin94@outlook.com>
2024-01-20 23:05:12 +0800

    update flake and ags config

--- xdg/config/ags/js/icons.js
+++ xdg/config/ags/js/icons.js
@@ -88,6 +88,7 @@ export default {
         next: '󰒭',
     },
     ui: {
+        colorpicker: 'color-select-symbolic',
         close: 'window-close-symbolic',
         info: 'info-symbolic',
         menu: 'open-menu-symbolic',
