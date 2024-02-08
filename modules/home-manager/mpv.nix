677a37ec5cc1113c62a5c35307cea29ac15ae2d8
wenjin <hewenjin94@outlook.com>
2024-02-08 22:28:36 +0800

    move mpv config to nix

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -6,14 +6,25 @@
   pkgs,
   ...
 }: {
-
-  home.packages = with pkgs; [
-    (mpv.override { scripts = [mpvScripts.mpris]; })
-  ];
-  home.file = {
-    ".config/mpv" = {
-      source = ../../xdg/config/mpv;
-      recursive = true;
-    };
+  # home.file = {
+  #   ".config/mpv" = {
+  #     source = ../../xdg/config/mpv;
+  #     recursive = true;
+  #   };
+  # };
+  programs.mpv = {
+    enable = true;
+    scripts = with pkgs; [
+      mpvScripts.mpris
+      mpvScripts.uosc
+      unstable.mpvScripts.mpv-cheatsheet
+      mpvScripts.visualizer
+      mpvScripts.cutter
+      mpvScripts.autoload
+      mpvScripts.thumbfast
+      mpvScripts.vr-reversal
+      mpvScripts.webtorrent-mpv-hook
+      mpvScripts.quality-menu
+    ];
   };
 }
