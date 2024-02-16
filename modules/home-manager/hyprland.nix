a15cbac48eb3ccd9f8a4509eec4c6efee4fbcb25
wenjin <hewenjin94@outlook.com>
2024-02-16 14:38:27 +0800

    update flake and hyprland version

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -265,7 +265,6 @@
             mouse_move_enables_dpms = true;
             key_press_enables_dpms = true;
             disable_hyprland_logo = true;
-            force_hypr_chan = true;
           };
           xwayland = {
             force_zero_scaling = true;
